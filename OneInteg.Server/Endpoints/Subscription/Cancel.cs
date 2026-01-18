2b017b158209af7ed8cb2cf0b2dee4246c43335c
Luis Puc <contacto@luispsarmiento.com>
2026-01-18 00:00:59 -0600

    Added: the cancel endpont (not implemented yet)

---
+++ OneInteg.Server/Endpoints/Subscription/Cancel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace OneInteg.Server.Endpoints.Subscription
+{
+    internal sealed class Cancel : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
