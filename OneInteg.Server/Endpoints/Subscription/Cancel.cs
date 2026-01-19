d8d0bfd0ab55190521949bf42843dafca0e4d4a9
Luis Puc <contacto@luispsarmiento.com>
2026-01-18 23:58:55 -0600

    Changed: return mock response for cancel endpoint

--- OneInteg.Server/Endpoints/Subscription/Cancel.cs
+++ OneInteg.Server/Endpoints/Subscription/Cancel.cs
@@ -1,11 +1,18 @@
 ﻿
+using Microsoft.AspNetCore.Mvc;
+
 namespace OneInteg.Server.Endpoints.Subscription
 {
     internal sealed class Cancel : IEndpoint
     {
         public void MapEndpoint(IEndpointRouteBuilder app)
         {
-            throw new NotImplementedException();
+            app.MapPost("/{t_id}/subscription/{reference}/cancel", 
+                async ([FromRoute(Name = "t_id")] Guid tenantId,
+                       [FromRoute(Name = "reference")] string reference) =>
+            {
+                return Results.Ok("Mock: Subscription cancelled");
+            });
         }
     }
 }
