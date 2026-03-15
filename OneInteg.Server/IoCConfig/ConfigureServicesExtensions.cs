7ea2df2112798f027e6dd50375eb97aab408446e
Luis Puc <contacto@luispsarmiento.com>
2026-03-14 21:43:44 -0600

    Added: service config to endpoints

--- OneInteg.Server/IoCConfig/ConfigureServicesExtensions.cs
+++ OneInteg.Server/IoCConfig/ConfigureServicesExtensions.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using MongoDB.Bson.Serialization.Serializers;
-using MongoDB.Bson.Serialization;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 using OneInteg.Server.DataAccess;
 using OneInteg.Server.Domain.Repositories;
 using OneInteg.Server.Domain.Services;
+using OneInteg.Server.Endpoints;
 using OneInteg.Server.Services;
+using System.Reflection;
 
 namespace OneInteg.Server.IoCConfig
 {
@@ -46,6 +49,36 @@ namespace OneInteg.Server.IoCConfig
                     baseUri, 
                     accessToken);
             });
+        }
+
+        public static IServiceCollection AddEndpoints(this IServiceCollection services, Assembly assembly)
+        {
+            ServiceDescriptor[] serviceDescriptors = assembly
+                .DefinedTypes
+                .Where(type => type is { IsAbstract: false, IsInterface: false } &&
+                               type.IsAssignableTo(typeof(IEndpoint)))
+                .Select(type => ServiceDescriptor.Transient(typeof(IEndpoint), type))
+                .ToArray();
+
+            services.TryAddEnumerable(serviceDescriptors);
+
+            return services;
+        }
+
+        public static IApplicationBuilder MapEndpoints(
+            this WebApplication app,
+            RouteGroupBuilder? routeGroupBuilder = null)
+        {
+            IEnumerable<IEndpoint> endpoints = app.Services.GetRequiredService<IEnumerable<IEndpoint>>();
+
+            IEndpointRouteBuilder builder = routeGroupBuilder is null ? app : routeGroupBuilder;
+
+            foreach (IEndpoint endpoint in endpoints)
+            {
+                endpoint.MapEndpoint(builder);
+            }
+
+            return app;
         }
     }
 }
